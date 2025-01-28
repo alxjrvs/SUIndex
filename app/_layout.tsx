d7976f13bcd3eaf6a774b92ff61e99ecc784997e
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-01-28 15:57:24 -0500

    EAS

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Drawer } from "expo-router/drawer";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from 'expo-splash-screen';
+SplashScreen.preventAutoHideAsync();
+
 
 export default function RootLayout() {
   return (
