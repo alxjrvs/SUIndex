c39fb9752f3c08c4781e787f58e78268d0458db8
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-01-31 00:50:12 -0500

    Hell over

--- components/ComponentContainer/ChassisStats.tsx
+++ components/ComponentContainer/ChassisStats.tsx
@@ -5,7 +5,6 @@ import { TechLevel } from '~/rulesReferences/types'
 import { ChassisStats as Stats } from '~/rulesReferences/types'
 import { PropsWithChildren } from 'react'
 
-type Position = 'up' | 'down'
 type Props = {
   stats: Stats | undefined
   up?: boolean
@@ -15,22 +14,29 @@ export function ChassisStats({ stats, up = false }: Props) {
   if (!stats) return null
 
   return (
-    <View
-      style={[
-        {
-          overflow: 'visible',
-          flexDirection: 'row',
-        },
-      ]}
-    >
-      <Stat up={up} label="Structure Pts." value={stats.structure_pts} />
-      <Stat up={up} label="Energy Pts." value={stats.energy_pts} />
-      <Stat up={up} label="Heat Cap." value={stats.heat_cap} />
-      <Stat up={up} label="System Slots" value={stats.system_slots} />
-      <Stat up={up} label="Module Slots" value={stats.module_slots} />
-      <Stat up={up} label="Cargo Cap." value={stats.cargo_cap} />
-      <Stat up={up} label="Tech Level" value={stats.tech_level} />
-      <Stat up={up} label="Salvage Value" value={stats.salvage_value} />
+    <View>
+      {!up && stats.notes && <AppText variant="bold">{stats.notes}</AppText>}
+      <View
+        style={[
+          up && {
+            paddingTop: 80,
+          },
+          {
+            overflow: 'visible',
+            flexDirection: 'row',
+          },
+        ]}
+      >
+        <Stat up={up} label="Structure Pts." value={stats.structure_pts} />
+        <Stat up={up} label="Energy Pts." value={stats.energy_pts} />
+        <Stat up={up} label="Heat Cap." value={stats.heat_cap} />
+        <Stat up={up} label="System Slots" value={stats.system_slots} />
+        <Stat up={up} label="Module Slots" value={stats.module_slots} />
+        <Stat up={up} label="Cargo Cap." value={stats.cargo_cap} />
+        <Stat up={up} label="Tech Level" value={stats.tech_level} />
+        <Stat up={up} label="Salvage Value" value={stats.salvage_value} />
+      </View>
+      {up && stats.notes && <AppText variant="bold">{stats.notes}</AppText>}
     </View>
   )
 }
@@ -51,8 +57,17 @@ function Stat({
         position: 'relative',
       }}
     >
-      <StatLabel up={up}>{label}</StatLabel>
-      <StatValue>{value}</StatValue>
+      {up ? (
+        <>
+          <StatLabel up={up}>{label}</StatLabel>
+          <StatValue>{value}</StatValue>
+        </>
+      ) : (
+        <>
+          <StatLabel up={up}>{label}</StatLabel>
+          <StatValue>{value}</StatValue>
+        </>
+      )}
     </View>
   )
 }
@@ -88,20 +103,23 @@ function StatLabel({ children, up }: PropsWithChildren<{ up?: boolean }>) {
     <View
       style={[
         !up && {
-          top: 14,
-          transform: [{ rotate: '45deg' }],
+          bottom: -95,
+          left: -75,
+          justifyContent: 'flex-end',
         },
         up && {
           top: 0,
-          transform: [{ rotate: '-45deg' }],
+          left: 9,
+          justifyContent: 'flex-start',
         },
         {
-          left: 9,
+          transform: [{ rotate: '-45deg' }],
+          transformOrigin: 'left',
+          flexDirection: 'row',
+          minWidth: 140,
           position: 'absolute',
           height: 14,
-          backgroundColor: colors.black,
           zIndex: 2,
-          transformOrigin: 'left',
           alignItems: 'center',
           alignContent: 'center',
           overflow: 'visible',
@@ -112,13 +130,14 @@ function StatLabel({ children, up }: PropsWithChildren<{ up?: boolean }>) {
         variant="heavy"
         numberOfLines={1}
         style={{
+          paddingRight: up ? 5 : 22,
+          paddingLeft: up ? 22 : 5,
           fontSize: 11,
           paddingTop: 1,
+          textAlign: 'right',
           backgroundColor: colors.black,
           color: colors.white,
           textTransform: 'uppercase',
-          paddingRight: 5,
-          paddingLeft: 22,
         }}
       >
         {children}
