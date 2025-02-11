d1846f7cba48e7be0ded9bb43a0fd61557fe202b
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-02-11 00:42:34 -0500

    WIP

--- components/Frame.tsx
+++ components/Frame.tsx
@@ -1,13 +1,13 @@
 import { StyleProp, View, ViewStyle } from 'react-native'
 import colors, { levelToBlue } from '~/colors'
 import { AppText } from './AppText'
-import { Header } from './ComponentContainer/Header'
 import { VerticalBar } from './ComponentContainer/VerticalBar'
 import MiniRollTableDisplay from './MiniRollTableDisplay'
 import { RollTable } from '~/context/reference/models/RollTable'
 import { PropsWithChildren } from 'react'
 import { TechLevel } from '~/context/reference/models/types'
 import { DataValue } from '~/types'
+import { DataList } from './DataList'
 
 type Props = {
   header: string | undefined
@@ -61,14 +61,62 @@ export function Frame({
     <View
       style={[{ backgroundColor: colors.SULightBlue, width: '100%' }, style]}
     >
-      <Header
-        backgroundColor={backgroundColor}
-        level={level}
-        header={header}
-        details={details}
-      >
-        {headerContent}
-      </Header>
+      <View style={{ backgroundColor, padding: 5, zIndex: 1 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+          {level && (
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                maxWidth: 35,
+                minWidth: 35,
+              }}
+            >
+              <AppText
+                variant="bold"
+                style={{
+                  color: colors.white,
+                  fontSize: 25,
+                  paddingRight: 5,
+                }}
+              >
+                {level}
+              </AppText>
+            </View>
+          )}
+          <View style={{ flex: 8 }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}
+            >
+              {header && (
+                <AppText
+                  variant="bold"
+                  style={{
+                    maxWidth: '80%',
+                    fontSize: 25,
+                    flexWrap: 'wrap',
+                    color: colors.white,
+                  }}
+                >
+                  {header}
+                </AppText>
+              )}
+              {headerContent}
+            </View>
+            <View style={{ minHeight: 15 }}>
+              <DataList
+                textColor={colors.white}
+                invert={backgroundColor === colors.black}
+                values={details || []}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
       <View
         style={{
           flexDirection: 'row',
